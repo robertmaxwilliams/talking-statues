bc4e298fbf26e56f6e1c1cfddd01f120aa912dbd
bionboy <lukefloden@gmail.com>
2020-03-04 17:15:14 -0500

    black formatting and update gitignore

--- probs.py
+++ probs.py
@@ -1,55 +1,62 @@
-
 import matplotlib.pyplot as plt
 from gpt_2_simple import *
 from gpt_2_simple.src import sample
 
 plt.xkcd()
 
-def my_sample_sequence(*, hparams, length, start_token=None,
-                    batch_size=None, context=None, temperature=1,
-                    top_k=0, top_p=0.0):
+
+def my_sample_sequence(
+    *,
+    hparams,
+    length,
+    start_token=None,
+    batch_size=None,
+    context=None,
+    temperature=1,
+    top_k=0,
+    top_p=0.0,
+):
     if start_token is None:
-        assert context is not None, 'Specify exactly one of start_token and context!'
+        assert context is not None, "Specify exactly one of start_token and context!"
     else:
-        assert context is None, 'Specify exactly one of start_token and context!'
+        assert context is None, "Specify exactly one of start_token and context!"
         context = tf.fill([batch_size, 1], start_token)
 
     def step(hparams, tokens, past=None):
-        lm_output = model.model(hparams=hparams, X=tokens,
-                                past=past, reuse=tf.compat.v1.AUTO_REUSE)
+        lm_output = model.model(
+            hparams=hparams, X=tokens, past=past, reuse=tf.compat.v1.AUTO_REUSE
+        )
 
-        logits = lm_output['logits'][:, :, :hparams.n_vocab]
-        presents = lm_output['present']
-        presents.set_shape(model.past_shape(
-            hparams=hparams, batch_size=batch_size))
+        logits = lm_output["logits"][:, :, : hparams.n_vocab]
+        presents = lm_output["present"]
+        presents.set_shape(model.past_shape(hparams=hparams, batch_size=batch_size))
         return {
-            'logits': logits,
-            'presents': presents,
+            "logits": logits,
+            "presents": presents,
         }
 
-    with tf.compat.v1.name_scope('sample_sequence'):
+    with tf.compat.v1.name_scope("sample_sequence"):
         # Don't feed the last context token -- leave that to the loop below
         # TODO: Would be slightly faster if we called step on the entire context,
         # rather than leaving the last token transformer calculation to the while loop.
         context_output = step(hparams, context[:, :-1])
-        print(f'!?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {context.shape}')
+        print(f"!?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {context.shape}")
 
         def body(past, prev, output, logis):
-        # def body(past, prev, output):
+            # def body(past, prev, output):
             next_outputs = step(hparams, prev[:, tf.newaxis], past=past)
-            logits = next_outputs['logits'][:, -1, :] / tf.cast(temperature, tf.float32)
+            logits = next_outputs["logits"][:, -1, :] / tf.cast(temperature, tf.float32)
             if top_p > 0.0:
                 logits = sample.top_p_logits(logits, p=top_p)
             else:
                 logits = sample.top_k_logits(logits, k=top_k)
-            samples = tf.random.categorical(
-                logits, num_samples=1, dtype=tf.int32)
-            print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {logits.shape}')
+            samples = tf.random.categorical(logits, num_samples=1, dtype=tf.int32)
+            print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {logits.shape}")
             return [
-                tf.concat([past, next_outputs['presents']], axis=-2),
+                tf.concat([past, next_outputs["presents"]], axis=-2),
                 tf.squeeze(samples, axis=[1]),
                 tf.concat([output, samples], axis=1),
-                tf.concat([logis, logits], axis=1)
+                tf.concat([logis, logits], axis=1),
             ]
 
         def cond(*args):
@@ -57,17 +64,19 @@ def my_sample_sequence(*, hparams, length, start_token=None,
 
         # _, _, tokens = tf.while_loop(
         _, _, tokens, logis = tf.while_loop(
-            cond=cond, body=body,
+            cond=cond,
+            body=body,
             maximum_iterations=length,
             loop_vars=[
-                context_output['presents'],
+                context_output["presents"],
                 context[:, -1],
                 context,
-                tf.zeros((5, 5, 50257))
+                tf.zeros((5, 5, 50257)),
             ],
             shape_invariants=[
-                tf.TensorShape(model.past_shape(
-                    hparams=hparams, batch_size=batch_size)),
+                tf.TensorShape(
+                    model.past_shape(hparams=hparams, batch_size=batch_size)
+                ),
                 tf.TensorShape([batch_size]),
                 tf.TensorShape([batch_size, None]),
             ],
@@ -77,49 +86,51 @@ def my_sample_sequence(*, hparams, length, start_token=None,
         # return tokens
         return tokens, logis
 
-def single_tokie (*, hparams, length, start_token=None,
-                    batch_size=None, context=None):
+
+def single_tokie(*, hparams, length, start_token=None, batch_size=None, context=None):
     if start_token is None:
-        assert context is not None, 'Specify exactly one of start_token and context!'
+        assert context is not None, "Specify exactly one of start_token and context!"
     else:
-        assert context is None, 'Specify exactly one of start_token and context!'
+        assert context is None, "Specify exactly one of start_token and context!"
         context = tf.fill([batch_size, 1], start_token)
 
     def step(hparams, tokens, past=None):
-        lm_output = model.model(hparams=hparams, X=tokens,
-                                past=past, reuse=tf.compat.v1.AUTO_REUSE)
+        lm_output = model.model(
+            hparams=hparams, X=tokens, past=past, reuse=tf.compat.v1.AUTO_REUSE
+        )
 
-        logits = lm_output['logits'][:, :, :hparams.n_vocab]
-        presents = lm_output['present']
-        presents.set_shape(model.past_shape(
-            hparams=hparams, batch_size=batch_size))
+        logits = lm_output["logits"][:, :, : hparams.n_vocab]
+        presents = lm_output["present"]
+        presents.set_shape(model.past_shape(hparams=hparams, batch_size=batch_size))
         return {
-            'logits': logits,
-            'presents': presents,
+            "logits": logits,
+            "presents": presents,
         }
-    return step(hparams, (context[:,:]), past=None)['logits']
 
-    
+    return step(hparams, (context[:, :]), past=None)["logits"]
 
-def generate2(sess,
-            run_name='run1',
-            checkpoint_dir='checkpoint',
-            model_name=None,
-            model_dir='models',
-            sample_dir='samples',
-            return_as_list=False,
-            truncate=None,
-            destination_path=None,
-            sample_delim='=' * 20 + '\n',
-            prefix=None,
-            seed=None,
-            nsamples=1,
-            batch_size=1,
-            length=1023,
-            temperature=0.7,
-            top_k=0,
-            top_p=0.0,
-            include_prefix=True):
+
+def generate2(
+    sess,
+    run_name="run1",
+    checkpoint_dir="checkpoint",
+    model_name=None,
+    model_dir="models",
+    sample_dir="samples",
+    return_as_list=False,
+    truncate=None,
+    destination_path=None,
+    sample_delim="=" * 20 + "\n",
+    prefix=None,
+    seed=None,
+    nsamples=1,
+    batch_size=1,
+    length=1023,
+    temperature=0.7,
+    top_k=0,
+    top_p=0.0,
+    include_prefix=True,
+):
     """Generates text from a model loaded into memory.
     Adapted from https://github.com/openai/gpt-2/blob/master/src/interactive_conditional_samples.py
     """
@@ -129,9 +140,9 @@ def generate2(sess,
     assert nsamples % batch_size == 0
 
     if nsamples == 1:
-        sample_delim = ''
+        sample_delim = ""
 
-    if prefix == '':
+    if prefix == "":
         prefix = None
 
     if model_name:
@@ -141,7 +152,7 @@ def generate2(sess,
 
     enc = encoder.get_encoder(checkpoint_path)
     hparams = model.default_hparams()
-    with open(os.path.join(checkpoint_path, 'hparams.json')) as f:
+    with open(os.path.join(checkpoint_path, "hparams.json")) as f:
         hparams.override_from_dict(json.load(f))
 
     if prefix:
@@ -151,25 +162,23 @@ def generate2(sess,
     np.random.seed(seed)
     tf.compat.v1.set_random_seed(seed)
 
-    logis = single_tokie(hparams=hparams, 
-                            length=1023 - len(context_tokens),
-                            start_token=None,
-                            context=context,
-                            batch_size=batch_size)
-    #logis = tf.math.sigmoid(logis)
+    logis = single_tokie(
+        hparams=hparams,
+        length=1023 - len(context_tokens),
+        start_token=None,
+        context=context,
+        batch_size=batch_size,
+    )
+    # logis = tf.math.sigmoid(logis)
 
+    f_me = sess.run(logis, feed_dict={context: batch_size * [context_tokens]})[0]
 
-    f_me = sess.run(logis, feed_dict={
-                    context: batch_size * [context_tokens]
-                    })[0]
-
-
-    print(f'f_me shaep: {f_me.shape}, context_tokens len: {len(context_tokens)}')
+    print(f"f_me shaep: {f_me.shape}, context_tokens len: {len(context_tokens)}")
     enc = encoder.get_encoder(checkpoint_path)
-    for offset in [-1,0,1]:
+    for offset in [-1, 0, 1]:
         for i, token in enumerate(context_tokens):
             try:
-                print(token, enc.decode([token]), f_me[i+offset, token])
+                print(token, enc.decode([token]), f_me[i + offset, token])
             except:
                 pass
 
@@ -179,13 +188,19 @@ def generate2(sess,
     print(f_me.shape)
     print(f_me)
     return
-                            
 
 
 sess = start_tf_sess()
 load_gpt2(sess)
 
 
-output = generate2(sess, prefix='The cat is the horse of this country', include_prefix=False, return_as_list=True, length=100, batch_size=1, nsamples=5)
+output = generate2(
+    sess,
+    prefix="The cat is the horse of this country",
+    include_prefix=False,
+    return_as_list=True,
+    length=100,
+    batch_size=1,
+    nsamples=5,
+)
 print(output)
-
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from random import randint as dice
 import time
 import markovify
 
-import gpt2_test 
+import gpt2_test
 
 app = Bottle()
 
@@ -13,6 +13,7 @@ with open("corpus.txt", encoding="utf8") as f:
     corpus = f.read()
 
 text_model = markovify.Text(corpus)
+
 
 def generate_from_text_model(text):
     gems = gpt2_test.generate_text(text)
@@ -25,8 +26,10 @@ def generate_from_text_model(text):
         return "sorry, model failure: " + str(e)
     return ret
 
+
 def random_color():
-    return "{:06x}".format(dice(0, 0xffffff))
+    return "{:06x}".format(dice(0, 0xFFFFFF))
+
 
 def colorize(text):
     words = text_model.word_split(text)
@@ -35,31 +38,34 @@ def colorize(text):
         ret += f"<span style='background-color:#{random_color()}'>{w}</span>"
     return ret
 
-@app.route('/hello')
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
 
-@app.route('/static/<filename>')
+@app.route("/static/<filename>")
 def server_static(filename):
-    return static_file(filename, root='./static/')
+    return static_file(filename, root="./static/")
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return static_file('foo.html', root='./')
+    return static_file("foo.html", root="./")
 
 
-@app.post('/generate')
+@app.post("/generate")
 def generate():
     time.sleep(1)
-    text = request.forms.get('text')
+    text = request.forms.get("text")
     return generate_from_text_model(text)
 
-@app.post('/highlight')
+
+@app.post("/highlight")
 def highlight():
     time.sleep(0.5)
-    text = request.forms.get('text')
+    text = request.forms.get("text")
     return "<p>" + colorize(text) + "</p>"
 
-run(app, host='localhost', port=8080)
+
+run(app, host="localhost", port=8080)
