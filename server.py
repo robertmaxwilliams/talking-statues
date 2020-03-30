2859223a05851178566fcd8f8cb6a53aa5d33a44
Robert Max Williams <robertmaxwilliams@gmail.com>
2020-03-30 14:40:47 -0400

    Added example webhook integration

--- server.py
+++ server.py
@@ -2,17 +2,11 @@ from bottle import static_file
 from bottle import Bottle, run, request
 from random import randint as dice
 import time
-import markovify
+import json
 
 import gpt2_test
 
 app = Bottle()
-
-# build text model
-with open("corpus.txt", encoding="utf8") as f:
-    corpus = f.read()
-
-text_model = markovify.Text(corpus)
 
 
 def generate_from_text_model(text):
@@ -67,5 +61,27 @@ def highlight():
     text = request.forms.get("text")
     return "<p>" + colorize(text) + "</p>"
 
+def print_red(text): 
+    print("\033[91m {}\033[00m".format(text))
+
+@app.post("/webhook")
+def webhook():
+    print("WEbhhook called!")
+    print_red(request.body.getvalue().decode('utf-8'))
+    body = json.loads(request.body.getvalue().decode('utf-8'))
+    if body["queryResult"]["queryText"] == "GOOGLE_ASSISTANT_WELCOME":
+        print("Welcome page, returning empty json object")
+        return '{}'
+    parameters = body['queryResult']['parameters']
+    story = 'Let me tell you a story. '
+    if parameters['subject_thing'] != '':
+        story += f"There was a {parameters['subject_thing']}. "
+    if parameters['subject_place'] != '':
+        story += f"This took place at {parameters['subject_place']}. "
+    if parameters['subject_event'] != '':
+        story += f"It was a great day, the day of a {parameters['subject_event']}. "
+    story += 'THE PLOT!'
+    json_text = f'{{"fulfillmentText": "{story}"}}'
+    return json_text
 
 run(app, host="localhost", port=8080)
