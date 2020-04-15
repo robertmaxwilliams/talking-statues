57e7055cb35337aeb5371c21aee885f76d1a4473
Luke Floden <lukefloden@gmail.com>
2020-04-15 20:54:37 +0000

    HIGHLIGHITNG WOKRS

--- colab/GCP_server_https.py
+++ colab/GCP_server_https.py
@@ -1,33 +1,86 @@
+from flask import Flask, render_template, send_from_directory, request
 import gpt_2_simple as gpt2
-import os
-import requests
-from random import randint as dice
-import random
-import time
 import json
+import os
+import random
+from random import randint as dice
+import requests
+import threading
+import time
+import colorsys
 
+from highlight import get_text_rankings
+
+app = Flask(__name__)
+
+#! Classes
 class bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-sess = None
 
-# Restart session if running this cell again
-if (sess != None):
-    gpt2.reset_session(sess)
+class LongRunner:
+    working = False
+    ready = False
+    story = None
+    asked = False
+    
+    def start(self, query_text):
+        run_start = time.time()
 
-sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, multi_gpu=False)
-# gpt2.load_gpt2(sess, multi_gpu=True)
+        # print(f'LongerRunner started: query = {query_text[100:]}')
+        print(f'LongerRunner started: query = {query_text}')
+
+        self.working = True
+        self.ready = False
+        self.asked = False
+
+        # time.sleep(20)
+        self.generate_story_from_query_text(query_text)
+
+        self.working = False
+        self.ready = True
+
+        run_end = time.time()
+        print(f'LongRunner done!, took {run_end - run_start} to finish')
+    
+    def get_result(self):
+        if self.ready:
+            self.ready = False
+            return (self.story, False)
+        elif not self.asked:
+            self.asked = True
+            return ('Okay, let me think of a story', True)
+        else:
+            return ('I\'m still thinking...', True)
+    
+    def generate_story_from_query_text(self, query_text):
+        prefix = random.choice(story_prefix_templates).format(query_text)
+        print(f'Rand: prefix = {prefix}')
+        
+        story = generate_text(prefix, 100, 1)[0]
+        # story = prefix[prefix.rfind('\'')+1:] + story
+
+        print('Returning a story:', story)
+
+        self.story = story
+
+
+#! Functions
 
 def generate_text(prefix, length, num_samples):
-    prefix = prefix[:length]
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess, multi_gpu=False)
+    
+    # prefix = prefix[:length]
+    print(f'Generate: prefix = {prefix}')
+    # print(len(prefix))
     output = gpt2.generate(
         sess,
         prefix=prefix,
@@ -36,173 +89,173 @@ def generate_text(prefix, length, num_samples):
         return_as_list=True,
         length=length,
         nsamples=num_samples,
-#         batch_size=num_samples,
+        # batch_size=num_samples,
     )
     output = [x[len(prefix):] for x in output]
+
+    gpt2.reset_session(sess)
+
     return output
-
-
-from flask_ngrok import run_with_ngrok
-from flask import Flask, render_template, send_from_directory, request
-import time
-
-import threading
-
-
-app = Flask(__name__)
-# run_with_ngrok(app) 
-
-
-# build text model
-with open("../static/corpus.txt", encoding="utf8") as f:
-    corpus = f.read()
 
 
 def generate_from_text_model(text, length=100, num_samples=1):
     gems = generate_text(text, length, num_samples)
-    ret = ""
+    ret = ''
     try:
         for i in range(num_samples):
-            ret += f"<div class='predictionBox'> <p><pre>{gems[i]}</pre></p> </div>"
+            ret += f'<div class=\'predictionBox\'> <p><pre>{gems[i]}</pre></p> </div>'
     except Exception as e:
-        print(f"FEEE\n{e}\nEEEEF")
-        return "sorry, model failure: " + str(e)
+        print(f'FEEE\n{e}\nEEEEF')
+        return 'sorry, model failure: ' + str(e)
     return ret
 
 
 def random_color():
-    return "{:06x}".format(dice(0, 0xFFFFFF))
+    return '{:06x}'.format(dice(0, 0xFFFFFF))
 
+def color_from_rank(rank):
+    if rank == 0:
+        h = 0.5
+    elif rank < 10:
+        h = 0.6
+    elif rank < 30:
+        h = 0.7
+    elif rank < 90:
+        h = 0.8
+    elif rank < 3*90:
+        h = 0.9
+    else:
+        h = 1
+    rgb = colorsys.hsv_to_rgb(h, 0.8, 0.8)
+    return ''.join(['{:02x}'.format(min(int(round(256*x)), 255)) for x in rgb]).upper()
 
 def colorize(text):
-    words = text.split(" ")
-    ret = ""
-    for w in words:
-        ret += f"<span style='background-color:#{random_color()}'>{w}</span>"
+    string_rank_pairs = get_text_rankings(text)
+    ret = ''
+    for word, rank in string_rank_pairs:
+        ret += f'<span style=\'background-color:#{color_from_rank(rank)}\'>{word}</span>'
     return ret
 
 
-@app.route("/static/<filename>")
-def server_static(filename):
-    return send_from_directory("../static/", filename)
+#! Routes
 
-
-@app.route("/")
+''' Default Route '''
+@app.route('/')
 def index():
-    return send_from_directory("../static/", "foo.html")
+    return send_from_directory('../static/', 'foo.html')
 
 
-@app.route("/generate", methods=["POST"])
+@app.route('/static/<filename>')
+def server_static(filename):
+    return send_from_directory('../static/', filename)
+
+
+@app.route('/generate', methods=['POST'])
 def generate():
-    generate_start= time.time()
+    generate_start = time.time()
     
-    text = request.form["text"]
-    print(f"Generate: prefix = {text}")
-    stories = generate_from_text_model(text, 20, num_samples=1)
+    text = request.form['text']
+    print(f'Generate POST: prefix = {text}')
+    stories = generate_from_text_model(text, 100, num_samples=1)
     
     generate_end = time.time()
-    print(f"Time to respond: {generate_end - generate_start}")
+    print(f'Time to respond: {generate_end - generate_start}')
     
     return stories
 
 
-@app.route("/highlight", methods=["POST"])
+@app.route('/highlight', methods=['POST'])
 def highlight():
     time.sleep(0.5)
-    text = request.form["text"]
-    print(f"Highlight: text = {text}")
-    return "<p>" + colorize(text) + "</p>"
+    text = request.form['text']
+    print(f'Highlight: text = {text}')
+    return '<p>' + colorize(text) + '</p>'
 
 def print_red(text): 
-    print("\033[91m {}\033[00m".format(text))
+    print('\033[91m {}\033[00m'.format(text))
+
+
+# exists so calling from thread is easilier
+def long_runner_helper(query_text):
+    print(f'Helper: query (len={len(query_text)}) = {query_text}')
+    long_runner.start(query_text)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    hook_start = time.time()
+    print('Webhhook called')
+
+    # Get relevant message data
+    body = request.get_json()
+    query_text = body['queryResult']['queryText']
+
+    # print(f'Webhook: query = {query_text[100:]}')
+    print(f'Webhook: query (len={len(query_text)}) = {query_text}')
+
+    # Handle welcom page request
+    if query_text == 'GOOGLE_ASSISTANT_WELCOME':
+        print('Welcome page, returning empty json object')
+        return '{}'
+
+    # Start long runner if needed
+    if not (long_runner.working or long_runner.ready):
+        x = threading.Thread(target=long_runner_helper, args=(query_text,))
+        x.start()
+
+    result, expect_response = long_runner.get_result()
+
+    hook_end = time.time()
+    print(f'WH Time to respond: {hook_end - hook_start}')
+
+    return json.dumps({
+        'fulfillmentText': result,
+        'payload': {
+            'google': {
+                'expectUserResponse': expect_response,
+                'richResponse': {
+                    'items': [{
+                        'simpleResponse': {
+                            'textToSpeech': result
+                            }
+                        }]
+                    }
+                }
+            }
+        })
+
 
 story_prefix_templates = [
-('The old man asked what I wanted to hear a story about. '
-'I pushed a coin his way and said "{}". He began his story, "Once upon a time,'),
-'''"Grandma, tell me a story!"
-"Of course dear, what about?"
-"{}"
-"Well, once upon a time''',
-'''We were all sitting around the campfire, and it was my turn to tell a story.
-"{}" one of them piped up from the other side of the fire.
-I had a perfect story, which I told without interruption: "A long, long time ago,'''
+        (
+            'The old man asked what I wanted to hear a story about. '
+            'I pushed a coin his way and said "{}". He began his story, "Once upon a time,'
+        ),
+        (
+            '"Grandma, tell me a story!"\n'
+            '"Of course dear, what about?"\n'
+            '"{}"\n'
+            '"Well, once upon a time'
+        ),
+        (
+            'We were all sitting around the campfire, and it was my turn to tell a story.\n'
+            '"{}" one of them piped up from the other side of the fire.\n'
+            'I had a perfect story, which I told without interruption: "A long, long time ago,'
+        )
 ]
 
 
 
+# Restart session if running this cell again
+# sess = None
+# if (sess != None):
+#    gpt2.reset_session(sess)
 
+#sess = gpt2.start_tf_sess()
+#gpt2.load_gpt2(sess, multi_gpu=False)
 
-class LongRunner:
-    working = False
-    ready = False
-    story = None
-    asked = False
-    def start(self, query_text):
-        self.working = True
-        self.ready = False
-        self.asked = False
-        time.sleep(20)
-        self.generate_story_from_query_text(query_text)
-        self.working = False
-        self.ready = True
-        print("done!")
-    def get_result(self):
-        if self.ready:
-            self.ready = False
-            return (self.story, False)
-        elif not self.asked:
-            self.asked = True
-            return ("Okay, let me think of a story", True)
-        else:
-            return ("I'm still thinking...", True)
-    def generate_story_from_query_text(self, query_text):
-        prefix = random.choice(story_prefix_templates).format(query_text)
-        story = generate_text(prefix, 5, 1)[0]
-        story = prefix[prefix.rfind('"')+1:] + story
-        print("Returning a story:", story)
-        self.story = story
 long_runner = LongRunner()
-
-# exists so calling from thread is easilier
-def long_runner_helper(query_text):
-    long_runner.start(query_text)
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    print("WEbhhook called!")
-    body = request.get_json()
-    query_text = body['queryResult']['queryText']
-    if query_text == "GOOGLE_ASSISTANT_WELCOME":
-        print("Welcome page, returning empty json object")
-        return '{}'
-    if not (long_runner.working or long_runner.ready):
-        x = threading.Thread(target=long_runner_helper, args=(query_text,))
-        x.start()
-    result, expect_response = long_runner.get_result()
-    return json.dumps(
-            {"fulfillmentText": result, 
-                "payload": {
-                    "google": {
-                        "expectUserResponse": expect_response,
-                        "richResponse": {
-                            "items": [
-                                {
-                                    "simpleResponse": {
-                                        "textToSpeech": result
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                })
-
-# context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-# context.use_privatekey_file('/etc/letsencrypt/live/sleepstorymachine.xyz/privkey.pem')
-# context.use_certificate_file('/etc/letsencrypt/live/sleepstorymachine.xyz/fullchain.pem')
 
 # context = ('/etc/letsencrypt/live/sleepstorymachine.xyz/fullchain.pem', '/etc/letsencrypt/live/sleepstorymachine.xyz/privkey.pem')
 context = ('cert.crt', 'priv.key')
 
-app.run(host="0.0.0.0", port=5000), ssl_context=context)
+app.run(host='0.0.0.0', port=5000, ssl_context=context)
 
---
+++ colab/highlight.py
@@ -0,0 +1,129 @@
+import os
+import json
+
+import tensorflow as tf
+import numpy as np
+
+from gpt_2_simple.src.sample import *
+from gpt_2_simple.src import model, sample, encoder
+import gpt_2_simple as gpt2
+
+def sample_sequence(*, hparams, length, start_token=None,
+                    batch_size=None, context=None, temperature=1,
+                    top_k=0, top_p=0.0):
+    if start_token is None:
+        assert context is not None, 'Specify exactly one of start_token and context!'
+    else:
+        assert context is None, 'Specify exactly one of start_token and context!'
+        context = tf.fill([batch_size, 1], start_token)
+    context_head = context[:,0:1]
+    context_tail = context[:,1:]
+
+    def step(hparams, tokens, past=None):
+        lm_output = model.model(hparams=hparams, X=tokens,
+                                past=past, reuse=tf.compat.v1.AUTO_REUSE)
+
+        logits = lm_output['logits'][:, :, :hparams.n_vocab]
+        presents = lm_output['present']
+        presents.set_shape(model.past_shape(
+            hparams=hparams, batch_size=batch_size))
+        return {
+            'logits': logits,
+            'presents': presents,
+        }
+
+    with tf.compat.v1.name_scope('sample_sequence'):
+        # Don't feed the last context token -- leave that to the loop below
+        # TODO: Would be slightly faster if we called step on the entire context,
+        # rather than leaving the last token transformer calculation to the while loop.
+        context_output = step(hparams, context_head[:, :-1])
+
+        def body(past, prev, context_head, context_tail, all_logits):
+            next_outputs = step(hparams, prev[:, tf.newaxis], past=past)
+            logits = next_outputs['logits'][:, -1, :] / tf.cast(temperature, tf.float32)
+            only_logits = logits
+            #if top_p > 0.0:
+            #    logits = top_p_logits(logits, p=top_p)
+            #else:
+            #    logits = top_k_logits(logits, k=top_k)
+            #samples = tf.random.categorical(
+            #    logits, num_samples=1, dtype=tf.int32)
+            return [
+                tf.concat([past, next_outputs['presents']], axis=-2),
+                context_tail[:,0],
+                tf.concat([context_head, context_tail[:,0:1]], axis=1),
+                context_tail[:,1:],
+                tf.concat([all_logits, tf.expand_dims(only_logits, 1)], axis=1)
+            ]
+
+        def cond(*args):
+            return True
+
+        past, prev, tokens, context_tail, all_logits = tf.while_loop(
+            cond=cond, body=body,
+            maximum_iterations=length,
+            loop_vars=[
+                context_output['presents'],
+                context[:, -1],
+                context_head,
+                context_tail,
+                tf.ones([batch_size, 0, 50257])
+            ],
+            shape_invariants=[
+                tf.TensorShape(model.past_shape(
+                    hparams=hparams, batch_size=batch_size)),#past?
+                tf.TensorShape([batch_size]), #prev?
+                tf.TensorShape([batch_size, None]),#context head
+                tf.TensorShape([batch_size, None]),#context tail
+                tf.TensorShape([batch_size, None, 50257]), #all logits
+            ],
+            back_prop=False,
+        )
+
+        return past, prev, tokens, all_logits
+
+def get_text_rankings(string):
+    '''takes string, returns pairs of (string, int) 
+    where the string is a part of the input and the int it it's gpt2 ranking with
+    0 being most likely'''
+    prefix = string
+    batch_size=1
+
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess, multi_gpu=False)
+    checkpoint_path = os.path.join('checkpoint', 'run1')
+    enc = encoder.get_encoder(checkpoint_path)
+    hparams = model.default_hparams()
+    with open(os.path.join(checkpoint_path, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+    context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
+    context_tokens = [50256] + enc.encode(prefix)
+    np.random.seed(42)
+    tf.compat.v1.set_random_seed(42)
+
+    past, prev, output, all_logits = sample_sequence(hparams=hparams, length=len(context_tokens)-1, start_token=None, batch_size=batch_size, context=context, temperature=1, top_k=0, top_p=0.0)
+    output = output[:, 1:]
+
+    pas, out, alt = sess.run([past, output, all_logits],
+                  feed_dict={context: batch_size * [context_tokens]})
+
+    text = enc.decode(out[0])
+    print(text)
+    print('generated tokens shape: ', out[0].shape)
+    print('pas shape: ', pas.shape)
+
+    def find_ranking(arr, index):
+        "finds the postions of the element at index if the array was sorted decreasing"
+        return (arr > arr[index]).sum()
+
+    string_rank_pairs = []
+    for i, token in enumerate(context_tokens[1:]):
+        logs = alt[0][i]
+        token_string = enc.decode([token])
+        token_ranking = find_ranking(logs, token)
+        print((token_string, token_ranking))
+        string_rank_pairs.append((token_string, token_ranking))
+
+    gpt2.reset_session(sess)
+
+    return string_rank_pairs
